7e1fdd26583906289a3a650fa0d8f93184ceda29
unknown <juliana_guonik@hotmail.com>
2022-06-09 19:10:28 -0300

    adding env config and sequelize-cli

---
+++ src/infrastructure/database/migrations/20220607223429-create-psicologo.js
@@ -0,0 +1,41 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('psicologos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nome: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      senha: {
+        type: Sequelize.STRING
+      },
+      apresentacao: {
+        type: Sequelize.STRING
+      },
+      bairro: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('psicologos');
+  }
+};
---
+++ src/infrastructure/database/seeders/20220607230057-create-psicologos.js
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('psicologos', [
+     
+      {
+        nome: 'João da Silva',
+        email: 'joaoaaas@mail.com',
+        senha: '123456',
+        apresentacao: 'sou d+',
+        bairro: 'Jardim dos Lírios',
+        createdAt: new Date(),
+        UpdatedAt: new Date(),
+        },
+        {
+          nome: 'JJ da Silva',
+          email: 'jo@mail.com',
+          senha: '123456',
+          apresentacao: 'sou d+',
+          bairro: 'Jardim dos Lírios',
+          createdAt: new Date(),
+          UpdatedAt: new Date(),
+          },
+          {
+            nome: 'ABC da Silva',
+            email: 'abcaaaeee@mail.com',
+            senha: '123456',
+            apresentacao: 'sou d+',
+            bairro: 'Jardim dos Lírios',
+            createdAt: new Date(),
+            UpdatedAt: new Date(),
+            }
+    ], {});
+
+      
+  },
+
+  async down (queryInterface, Sequelize) {
+
+    await queryInterface.bulkDelete('psicologos', null, {});
+  }
+};
